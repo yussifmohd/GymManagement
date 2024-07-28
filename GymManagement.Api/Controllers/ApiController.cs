dfea578bb93c57063a5d1a45fb876f2fe798e26c
Yousef Muhammed <yussifmohd17@gmail.com>
2024-07-28 11:08:43 +0300

    Fix Project Name

---
+++ GymManagement.Api/Controllers/ApiController.cs
@@ -0,0 +1,51 @@
+﻿using ErrorOr;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System.Reflection.Metadata.Ecma335;
+
+namespace GymManagment.Api.Controllers
+{
+    [ApiController]
+    public class ApiController : ControllerBase
+    {
+        protected IActionResult Problem(List<Error> errors)
+        {
+            if(errors.Count is 0)
+            {
+                return Problem();
+            }
+
+            if(errors.All(error => error.Type == ErrorType.Validation)) //if all are validarion erros
+            {
+                return ValidationProblem();
+            }
+
+            return Problem(errors[0]);
+        }
+
+        protected IActionResult Problem(Error error)
+        {
+            var statusCode = error.Type switch
+            {
+                ErrorType.Conflict => StatusCodes.Status409Conflict,
+                ErrorType.Validation => StatusCodes.Status400BadRequest,
+                ErrorType.NotFound => StatusCodes.Status404NotFound,
+                _ => StatusCodes.Status500InternalServerError
+            };
+
+            return Problem(statusCode: statusCode, detail: error.Description);
+        }
+
+        protected IActionResult ValidationProblem(List<Error> errors)
+        {
+            var modelStateDictonary = new ModelStateDictionary();
+
+            foreach (var error in errors)
+            {
+                modelStateDictonary.AddModelError(error.Code, error.Description);
+            }
+
+            return ValidationProblem(modelStateDictonary);
+        }
+    }
+}
