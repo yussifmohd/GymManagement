67da09b0989f41982c15d64a1d58936e8a221074
Yousef Muhammed <yussifmohd17@gmail.com>
2024-08-07 09:30:42 +0300

    test!: Add some unit tests to the validation behavior in our application layer

---
+++ GymManagment.Application.UnitTests/Common/Behaviors/ValidationBehaviotTests.cs
@@ -0,0 +1,80 @@
+﻿using ErrorOr;
+using FluentAssertions;
+using FluentValidation;
+using FluentValidation.Results;
+using GymManagement.Application.Common.Behavior;
+using GymManagement.Application.Gyms.Command.CreateGym;
+using GymManagment.Domain.Gyms;
+using MediatR;
+using NSubstitute;
+using TestsCommon.Gyms;
+
+namespace GymManagment.Application.UnitTests.Common.Behaviors
+{
+    public class ValidationBehavotTests
+    {
+        private readonly ValidationBehavior<CreateGymCommand, ErrorOr<Gym>> _validationBehavior;
+        private readonly IValidator<CreateGymCommand> _mockValidator;
+        private readonly RequestHandlerDelegate<ErrorOr<Gym>> _mockNextBehavior;
+
+        public ValidationBehavotTests()
+        {
+            //Create next behavior(mock)
+            _mockNextBehavior = Substitute.For<RequestHandlerDelegate<ErrorOr<Gym>>>();
+
+            //Create validator (mock)
+            _mockValidator = Substitute.For<IValidator<CreateGymCommand>>();
+
+            //Create validation behavior (System under test)
+            _validationBehavior = new ValidationBehavior<CreateGymCommand, ErrorOr<Gym>>(_mockValidator);
+        }
+
+        [Fact]
+        public async Task InvokeBehavior_WhenValidatorResultIsValid_ShouldInvokeNextBehavior()
+        {
+            //Arrange
+            var createGymRequest = GymCommandFactory.CreateCreateGymCommand();
+            var gym = GymFactory.CreateGym();
+
+            _mockValidator
+               .ValidateAsync(createGymRequest, Arg.Any<CancellationToken>())
+               .Returns(new ValidationResult());
+
+            _mockNextBehavior.Invoke().Returns(gym);
+
+            //Act
+            //Invoke Behavior
+            var result = await _validationBehavior.Handle(createGymRequest, _mockNextBehavior, default);
+
+            //Assert
+            //Result from invoking the behavior, was the result returned by next behavior
+            result.IsError.Should().BeFalse();
+            result.Value.Should().BeEquivalentTo(gym);
+        
+        }
+
+        [Fact]
+        public async Task InvokeBehavior_WhenValidatorResultIsNotValid_ShouldReturnListOfErrors()
+        {
+            //Arrange
+            var createGymRequest = GymCommandFactory.CreateCreateGymCommand();
+            List<ValidationFailure> validationFailures = [new(propertyName: "prop", errorMessage: "failed prop")];
+
+            _mockValidator
+               .ValidateAsync(createGymRequest, Arg.Any<CancellationToken>())
+               .Returns(new ValidationResult(validationFailures));
+
+            //Act
+            //Invoke Behavior
+            var result = await _validationBehavior.Handle(createGymRequest, _mockNextBehavior, default);
+
+            //Assert
+            //Result from invoking the behavior, was the result returned by next behavior
+            result.IsError.Should().BeTrue();
+            result.FirstError.Code.Should().Be("prop");
+            result.FirstError.Description.Should().Be("failed prop");
+
+        }
+
+    }
+}
---
+++ TestsCommon/Gyms/GymCommandFactory.cs
@@ -0,0 +1,18 @@
+﻿using GymManagement.Application.Gyms.Command.CreateGym;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestsCommon.TestConstants;
+
+namespace TestsCommon.Gyms
+{
+    public static class GymCommandFactory
+    {
+        public static CreateGymCommand CreateCreateGymCommand(string name = Constants.Gym.Name, Guid? subscriptionId = null)
+        {
+            return new CreateGymCommand(name , SubscriptionId: subscriptionId ?? Constants.Subscriptions.Id);
+        }
+    }
+}
